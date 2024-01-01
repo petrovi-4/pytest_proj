863457a6c4d79dba3a0000237aa5c02af842fea7
petrovi4 <40.10.444@gmail.com>
2024-01-01 14:16:50 +0300

    test-cov 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,12 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+
+def test_empty_list_slice():
+    assert arrs.my_slice([], 0) == []
+
+
+def test_start_is_none():
+    assert arrs.my_slice([1, 2, 3], None) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], None, 2) == [1, 2]
